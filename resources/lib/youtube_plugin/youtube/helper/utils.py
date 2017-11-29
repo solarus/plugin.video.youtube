3bad2ccdb7aa684b2aa276b481f019db42f91c67
anxdpanic <anxdpanic@users.noreply.github.com>
2017-11-29 18:15:56 -0500

    unofficial patch

--- resources/lib/youtube_plugin/youtube/helper/utils.py
+++ resources/lib/youtube_plugin/youtube/helper/utils.py
@@ -198,7 +198,7 @@ def update_video_infos(provider, context, video_id_dict, playlist_item_id_dict=N
         video_item.live = True if snippet.get('liveBroadcastContent') == 'live' else False
 
         # set mediatype
-        video_item.set_mediatype('video')  # using video
+        video_item.set_mediatype('episode')  # using episode since all setContent is currently episode as well
 
         # duration
         if not video_item.live and use_play_data and play_data.get('total_time'):
